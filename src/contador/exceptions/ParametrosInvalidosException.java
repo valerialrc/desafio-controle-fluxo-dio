4f9fbe4c5153d61fa9a199b48f0c1744719cd3d7
Valéria Carneiro <valerialribeiroc@gmail.com>
2024-06-17 19:03:19 -0300

    Adds solution

---
+++ src/contador/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package contador.exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+  public ParametrosInvalidosException(String mensagem) {
+      super(mensagem);
+  }
+}
